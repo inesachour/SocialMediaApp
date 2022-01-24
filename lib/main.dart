92a9769279b0645e3a95d3dfcb86a603346adf84
SametMohamedRayen <82988693+SametMohamedRayen@users.noreply.github.com>
2022-01-24 22:12:31 +0100

    Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './auth/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Login',
+      /*theme: ThemeData(
+        colorScheme: ColorScheme(
+
+        )
+      ),*/
+      home: LoginPage(),
     );
   }
 }
