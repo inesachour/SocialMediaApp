d358e6ca271b5776538c3d7d444ba5175fcc9a34
unknown <inesachour@insat.u-carthage.tn>
2022-02-17 16:40:20 +0100

    adding colors

--- lib/screens/feed/details.dart
+++ lib/screens/feed/details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Details extends StatefulWidget {
   const Details({Key? key}) : super(key: key);
@@ -13,27 +14,26 @@ class _DetailsState extends State<Details> {
   @override
   Widget build(BuildContext context) {
 
-
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor ,
+      backgroundColor: Theme.of(context).colorScheme.background ,
       appBar:AppBar(
         elevation: 0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        backgroundColor: Theme.of(context).colorScheme.primary,
         leading: IconButton(
-          icon :Icon(Icons.arrow_back_ios_outlined, color: Colors.grey,),
+          icon :Icon(Icons.arrow_back_ios_outlined, color: Colors.white,),
           splashColor: Colors.transparent,
           highlightColor: Colors.transparent,
           onPressed: (){},
         ),
-        title: Text("Blog Post",style: TextStyle(color: Colors.grey),),
+        title: Text("Blog Post",style: TextStyle(color: Colors.white),),
         centerTitle: true,
         actions: [
           IconButton(
             onPressed: (){},
-            icon: Icon(Icons.more_horiz, color: Colors.grey,),
+            icon: Icon(Icons.more_horiz, color: Colors.white,),
             splashColor: Colors.transparent,
             highlightColor: Colors.transparent,
           ),
@@ -44,9 +44,9 @@ class _DetailsState extends State<Details> {
         child: Column(
           children: [
             SizedBox(height: height*0.02,),
-            AccountInfos(height,width),
+            AccountInfos(height,width,context),
             SizedBox(height: height*0.03,),
-            Post(width,height),
+            Post(width,height,context),
             SizedBox(height: height*0.03,),
           ],
         ),
@@ -56,11 +56,12 @@ class _DetailsState extends State<Details> {
         type: BottomNavigationBarType.fixed,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+
         items: [
-          PostStatus(CupertinoIcons.heart, "1k" ,""),
-          PostStatus(CupertinoIcons.chat_bubble, "500" ,""),
-          PostStatus(CupertinoIcons.arrowshape_turn_up_right, "1k" ,""),
+          PostStatus(CupertinoIcons.heart_fill, "1k" ,""),
+          PostStatus(CupertinoIcons.chat_bubble_fill, "500" ,""),
+          PostStatus(CupertinoIcons.arrowshape_turn_up_right_fill, "1k" ,""),
           PostStatus(CupertinoIcons.bookmark_fill, "" ,""),
         ],
       ),
@@ -78,21 +79,21 @@ class _DetailsState extends State<Details> {
           backgroundColor: MaterialStateProperty.all(Colors.transparent),
         ),
         onPressed: (){},
-        icon: Icon(icon,color: Colors.grey,),
-        label: Text(text,style: TextStyle(color: Colors.grey),),
+        icon: Icon(icon,color: Colors.white,),
+        label: Text(text,style: TextStyle(color: Colors.white),),
       ),
     );
   }
 
 
-  Widget AccountInfos(height,width){
+  Widget AccountInfos(height,width,context){
     return Row(
       children: [
         Expanded(
           flex: 1,
           child: CircleAvatar(
             radius: width < 1000? width*0.08 : 75,
-            backgroundImage: Image.asset("assets/logos/google.jpg").image,
+            backgroundImage: Image.asset("insert").image,
             backgroundColor: Colors.transparent,
           ),
         ),
@@ -104,8 +105,9 @@ class _DetailsState extends State<Details> {
             Text(
               "Name walla dra chneya",
               style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: width< 800? width*0.05 : 35,
+                fontWeight: FontWeight.w500,
+                fontSize: width< 800? width*0.047 : 35,
+                color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
             SizedBox(height: 5,),
@@ -114,13 +116,13 @@ class _DetailsState extends State<Details> {
                 text: 'Topic : ',
                 style: TextStyle(
                     fontSize: width < 800 ? width*0.032 : 23,
-                    color: Colors.grey
+                    color: Theme.of(context).colorScheme.secondary,
                 ),
                 children: [
                   TextSpan(
                     text: 'Test Test',
                     style: TextStyle(
-                      color: Colors.black,
+                      color: Theme.of(context).colorScheme.onBackground,
                       fontSize: width< 800? width*0.038 : 28,
                     ),
                   ),
@@ -134,38 +136,49 @@ class _DetailsState extends State<Details> {
   }
 
 
-  Widget Post(width,height){
+  Widget Post(width,height,context){
     return Padding(
       padding: EdgeInsets.symmetric(horizontal : width*0.05),
       child: Column(
         children: [
           SizedBox(
             width: double.infinity,
+
             child: Text(
-              "Title title title title title title title title title title title title title",
+              "Lorem ipsum dolor sit",
               style: TextStyle(
-                  fontSize: width< 800? width*0.05 : 35,
-                  fontWeight: FontWeight.w500
+                fontSize: width< 800? width*0.060 : 35,
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
           ),
-          SizedBox(height: 10,),
+          SizedBox(height: 20,),
           SizedBox(
             width: double.infinity,
             child: Text(
-              "Title title title title title title title title title title title title title Title title title title title title title title title title title title title Title title title title title title title title title title title title title Title title title title title title title title title title title title title Title title title title title title title title title title title title title",
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus mattis rhoncus urna neque viverra. Duis convallis convallis tellus id interdum velit. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Sapien nec sagittis aliquam malesuada bibendum arcu. Morbi tristique senectus et netus et malesuada fames ac turpis. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Elementum tempus egestas sed sed. Quis hendrerit dolor magna eget est lorem ipsum. Tortor at risus viverra adipiscing at. Ullamcorper velit sed ullamcorper morbi tincidunt ornare.",
               style: TextStyle(
-                fontSize: width< 800? width*0.03 : 22,
+                fontSize: width< 800? width*0.037 : 22,
                 height: 1.5,
-                color: Colors.grey,
+                color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
           ),
           SizedBox(height: 25,),
-          Image.asset(
-            'assets/logos/google.jpg',
-            width: width*0.6,
-            fit: BoxFit.contain,
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 0.8,
+                color: Colors.grey,
+              )
+            ),
+            child: Image.asset(
+              'inset',
+              width: width*0.85,
+              fit: BoxFit.contain,
+
+            ),
           ),
         ],
       ),
